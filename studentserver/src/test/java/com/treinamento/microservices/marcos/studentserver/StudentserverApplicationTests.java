b86e94e17e7cd210a11abdeb83d49197a5d2348b
marcos.cavalheiro <marcos.cavalheiro@castgroup.com.br>
2019-10-31 08:49:31 -0300

    Developing studentserver

---
+++ studentserver/src/test/java/com/treinamento/microservices/marcos/studentserver/StudentserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.microservices.marcos.studentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
